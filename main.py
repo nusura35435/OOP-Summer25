f79e2c06e7ef09554f5626972979974e81549e30
nusura35435 <niyomukizanusura@gmail.com>
2025-03-06 12:15:36 +0000

    Add Person class

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ p1 = Person("nusura", 22)
 
 print(p1.name)
 print(p1.age)
+
+
+class Person:
+  def __init__(self, name, age, address):
+    self.name = name
+    self.age = age
+    self.address = address
+
+p1 = Person("nusura", 22, "libijska16")
+
+print(p1.name)
+print(p1.age)
+print(p1.address)

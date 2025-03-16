092f629e849aa8652de5f95b07d22d6bd4d83625
nusura35435 <niyomukizanusura@gmail.com>
2025-03-16 10:08:07 +0000

    Add new file

---
+++ students.py
@@ -0,0 +1,42 @@
+student1= {
+    'first_name':'Nusura',
+    'last_name':'Niyomukiza',
+    'index_number':'35435',
+    'nationality':'Rwandan',
+    'starting_date':'01-10-2024',
+    'courses':["mathematics","polish","computer science", "English"]
+}
+print(student1["first_name"])
+print(student1["last_name"])
+print(student1["index_number"])
+print(student1["nationality"])
+print(student1["starting_date"])
+print(student1["courses"])
+student2= {
+    'first_name':'Leina',
+    'last_name':'Bengnen',
+    'index_number':'35534',
+    'nationality':'american',
+    'starting_date':'01-10-2024',
+    'courses':["mathematics","polish","computer science", "English"]
+    }
+print(student2["first_name"])
+print(student2["last_name"])
+print(student2["index_number"])
+print(student2["nationality"])
+print(student2["starting_date"])
+print(student2["courses"])
+student1= {
+    'first_name':'Aleksandra',
+    'last_name':'Agata',
+    'index_number':'35443',
+    'nationality':'Polish',
+    'starting_date':'01-10-2024',
+    'courses':["mathematics","polish","computer science", "English"]
+}
+print(student1["first_name"])
+print(student1["last_name"])
+print(student1["index_number"])
+print(student1["nationality"])
+print(student1["starting_date"])
+print(student1["courses"])

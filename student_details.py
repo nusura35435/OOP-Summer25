d4ff35f816d859ba48a4e08202a9450c587eb2d0
nusura35435 <niyomukizanusura@gmail.com>
2025-03-16 09:34:10 +0000

    Add new file

---
+++ student_details.py
@@ -0,0 +1,8 @@
+student1= {
+    'first_name':'Nusura',
+    'last_name':'Niyomukiza',
+    'index_number':'35435',
+    'nationality':'Rwandan',
+    'starting_date':'01-10-2024',
+    'courses':["mathematics","polish","computer science","English"]
+    }

fd1d6d8ca076e73a596b18ae38903d6e6c57ce3e
nusura35435 <niyomukizanusura@gmail.com>
2025-03-17 21:59:04 -1100

    Update students.py

--- students.py
+++ students.py
@@ -1,10 +1,15 @@
-# List of students
-students = ["nusura niyomukiza: 35434", "Leina Bengnen: 35443", "Alenksandra Agata: 35543"]
+# List of students with their dictionaries
+students = [
+    {"name": "nusura niyomukiza", "index": 35435},
+    {"name": "Leina bembnen", "index": 35443},
+    {"name": "Aleksandra agata", "index": 35453}
+]
 
-# Loop through the list and display each student's index and name
+# Loop through the list and display each student's name and index number
 print("Student List:")
-for name, index in enumerate(students):
-    print(f"{name}: {index}")
+for student in students:
+    print(f"Name: {student['name']}, Index: {student['index']}")
+
 
 def add_student(student_list, new_student):
     student_list.append(new_student)

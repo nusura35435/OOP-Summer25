0832df70235a61c5bb2b5b6cfe04d8e5db334b3f
nusura35435 <niyomukizanusura@gmail.com>
2025-03-20 11:38:21 +0000

    Add new file

---
+++ animals.py
@@ -0,0 +1,32 @@
+animals = [
+    {
+        'name': 'Lion',
+        'group': 'Mammal',
+        'number_of_legs': 4,
+        'skills': ['hunting', 'roaring', 'running']
+    },
+    {
+        'name': 'Eagle',
+        'group': 'Bird',
+        'number_of_legs': 2,
+        'skills': ['flying', 'sharp vision', 'hunting']
+    },
+    {
+        'name': 'Shark',
+        'group': 'Fish',
+        'number_of_legs': 0,
+        'skills': ['swimming', 'hunting', 'sensing blood']
+    },
+    {
+        'name': 'Frog',
+        'group': 'Amphibian',
+        'number_of_legs': 4,
+        'skills': ['jumping', 'swimming', 'croaking']
+    },
+    {
+        'name': 'Spider',
+        'group': 'Arachnid',
+        'number_of_legs': 8,
+        'skills': ['web making', 'climbing', 'trapping prey']
+    }
+]

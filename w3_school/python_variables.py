2ede3d7449e9bc070533c2705c037b954138da7f
nusura35435 <niyomukizanusura@gmail.com>
2025-04-03 10:36:12 +0000

    Add new file

---
+++ w3_school/python_variables.py
@@ -0,0 +1,22 @@
+x = 5
+y = "nusura"
+print(x)
+print(y)
+
+x = 35435
+y = "nusura"
+print(type(x))
+print(type(y))
+
+my_name = "nusura"
+print(my_name)
+
+l,n,b = "blue","white","black"
+print(l)
+print(n)
+print(b)
+
+l=n=b = "black"
+print(l)
+print(n)
+print(b)

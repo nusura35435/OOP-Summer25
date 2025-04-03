eb3476d49b2669a26c2bda5d0f83d4a2da5e3413
nusura35435 <niyomukizanusura@gmail.com>
2025-04-03 11:18:20 +0000

    Add new file

---
+++ w3_school/python_data_types.py
@@ -0,0 +1,24 @@
+n = 5
+print(type(n))
+
+x = 1
+y = 6.54646464673737366564773
+z = 1j 
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 1 #int
+yc= 1.09
+#covert int to float
+a = float(x)
+print(x)
+print(a)

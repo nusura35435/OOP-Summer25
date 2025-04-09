6a01417dc56d009650c15ccb5162e4c3dfc6d740
nusura35435 <niyomukizanusura@gmail.com>
2025-04-09 08:57:14 +0000

    Add new file

---
+++ my_assignment.py
@@ -0,0 +1,31 @@
+# print a hello world on the screen
+print('Hello')
+
+# create a variable for your name 
+name = "Nusura"
+print(name)
+# variable names
+myvar = "Nusura"
+my_var = "Nusura"
+print(myvar)
+print(my_var)
+#python numbers
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+#Convert from one type to another
+x = 1    # int
+#convert from int to float:
+a = float(x)
+print(a)
+# python commets
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")

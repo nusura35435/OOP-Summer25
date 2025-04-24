200b01b70919428f99027f675d17e908e1295fc5
nusura35435 <niyomukizanusura@gmail.com>
2025-04-24 11:22:25 +0000

    Add new file

---
+++ w3_school/spython_string_type.py
@@ -0,0 +1,25 @@
+#change to upper and lower  letter
+a = "i am nusura"
+print(a.upper())
+b = "I AM NUSURA"
+print(b.lower())
+# remove white space
+c = "i am nusura"
+print(c.strip())
+#replace letter 
+d = "i am tusura"
+print(d.replace("t","n"))
+#Merge variable
+e = " I am"
+f = " Nusura"
+j = e + f
+print(j)
+j = e+" "+f
+print(j)
+# add string and variable
+age = 20
+txt = f"my name is nusura, i am {age}"
+print(txt)
+price = 50 
+txt = f"the cloth is {price} euros"
+print(txt)
